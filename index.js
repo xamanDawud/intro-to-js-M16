2475dfc80de166b913c26072fe469c625ff895cd
xamanDawud <ahmeddaudking100@gmail.com>
2023-07-29 12:00:46 +0600

    some javascript type explored

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("Hello Xaman Dawud");
+// Numeric Type
+var price = 2300;
+var pen_price = 24;
+var key = 23;
+console.log(price, pen_price, key);
+
+// String Type
+var person = "Komor Uddin";
+var location = "Andor killa bandorban";
+var crush = "Alia Bhatt";
+var special = `Whtay type of topic will be discussed ${25} or grater topic will come`;
+console.log(special);
+
+// Boolean Type
+var ready = true;
+var isCompleteWork = false;
+console.log(ready);
